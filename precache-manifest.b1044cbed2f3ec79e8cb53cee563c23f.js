d7754f4c5a112ec741a06e8879ccca20e342f178
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-09-16 12:13:06 +0300

    Updates

---
+++ precache-manifest.b1044cbed2f3ec79e8cb53cee563c23f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf437404fe2b120cca0d51886f67d8b",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "2eafae0768f8b032d195",
+    "url": "/portfolio-react/static/css/main.2eef19b0.chunk.css"
+  },
+  {
+    "revision": "4e6ade66d4dc302c3b22",
+    "url": "/portfolio-react/static/js/2.a04afccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.a04afccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eafae0768f8b032d195",
+    "url": "/portfolio-react/static/js/main.67ba671c.chunk.js"
+  },
+  {
+    "revision": "f16589a8831e993e10da",
+    "url": "/portfolio-react/static/js/runtime-main.5febc6f1.js"
+  }
+]);
