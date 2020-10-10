10f183d4a8d5ac177134b595acc060e6e4f310ea
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-10-10 14:30:09 +0300

    new style for contacts

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import styleContainer from './../common/styles/Сontainer.module.css'
 import style from './Contacts.module.scss'
 import {Title} from "../common/components/title/Title";
+import axios from 'axios';
+
 
 export const Contacts = () => {
+    const [personalDataUser,setPersonalDataUser] = useState({
+        name:'',
+        email:'',
+        number: '',
+        message:'',
+    })
+
+
+    const handleNameChange = (e) => {
+        setPersonalDataUser({...personalDataUser,name: e.currentTarget.value})
+    }
+    const handleEmailChange = (e) => {
+        setPersonalDataUser({...personalDataUser,email: e.currentTarget.value})
+    }
+    const handleNumberChange = (e) => {
+        setPersonalDataUser({...personalDataUser,number: e.currentTarget.value})
+    }
+    const handleMessageChange = (e) => {
+        setPersonalDataUser({...personalDataUser,message: e.currentTarget.value})    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('https://message-nodejs-server.herokuapp.com/sendMessage',personalDataUser)
+    }
+
     return (
         <div className={`${styleContainer.container} ${style.contactsBlock}`}>
             <Title title={'Contact'}/>
-            <form className={style.form}>
-                <input type="text" className={style.input}></input>
-                <input type="text" className={style.input}></input>
-                <textarea className={style.textarea}></textarea>
+            <form onSubmit={handleSubmit} className={style.form}>
+                <input placeholder={'Name'}
+                       type="text"
+                       className={style.input}
+                       value={personalDataUser.name}
+                       onChange={handleNameChange}
+                />
+                <input placeholder={'Email'}
+                       type="text"
+                       className={style.input}
+                       value={personalDataUser.email}
+                       onChange={handleEmailChange}
+                    /><input placeholder={'Number'}
+                       type="text"
+                       className={style.input}
+                       value={personalDataUser.number}
+                       onChange={handleNumberChange}
+                />
+                <textarea placeholder={'Message'}
+                          className={style.textarea}
+                          value={personalDataUser.message}
+                          onChange={handleMessageChange}
+                />
+                <button className={style.button}>Send message</button>
             </form>
-            <button className={style.button}>Отправить</button>
         </div>
     )
 }
