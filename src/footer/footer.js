6abf48f8995887ab7f107e7a05bc89f080d726a4
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-10-15 15:01:04 +0300

    add icons for footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,6 +1,14 @@
 import React from "react";
 import styleContainer from './../common/styles/Сontainer.module.css'
 import style from './footer.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import {faFacebook} from '@fortawesome/free-brands-svg-icons'
+import {faVk} from '@fortawesome/free-brands-svg-icons'
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faTelegram} from '@fortawesome/free-brands-svg-icons'
+
 
 
 export const Footer = () => {
@@ -9,10 +17,11 @@ export const Footer = () => {
             <div className={style.footerBlock}>
                 <div className={`${styleContainer.container} ${style.footerBlockContainer}`}>
                     <ul className={style.socNetworkContainer}>
-                        <li><a href={'#'}>Facebook</a></li>
-                        <li><a href={'#'}>VK</a></li>
-                        <li><a href={'#'}>Instagram</a></li>
-                        <li><a href={'#'}>LinkedIn</a></li>
+                        <li><a href={'#'}><FontAwesomeIcon className={style.icon} icon={faFacebook } /></a></li>
+                        <li><a href={'#'}><FontAwesomeIcon className={style.icon} icon={faVk } /></a></li>
+                        <li><a href={'#'}><FontAwesomeIcon className={style.icon} icon={faInstagram} /></a></li>
+                        <li><a href={'#'}><FontAwesomeIcon className={style.icon} icon={faLinkedin} /></a></li>
+                        <li><a href={'#'}><FontAwesomeIcon className={style.icon} icon={faTelegram} /></a></li>
                     </ul>
                     <p className={style.p}>© 2020 All Rights Reserved.</p>
                 </div>
